186aaeef8f0fdaff623323089354910ee28653d5
breno-felipe <brendo.filipe2050@gmail.com>
2021-11-15 22:10:06 -0300

    clenup started

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `brenofelipsdev`,
+    description: `Meu blog para compartilhar conteúdos sobre tecnologia.`,
+    author: `Breno Felipe`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
   plugins: [
