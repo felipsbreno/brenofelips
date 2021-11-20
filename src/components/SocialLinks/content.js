e0f9896f4d221815675e594bade7fee0cef1bc21
breno-felipe <brendo.filipe2050@gmail.com>
2021-11-20 17:49:30 -0300

    feat(sociallinks): add component SocialLinks

---
+++ src/components/SocialLinks/content.js
@@ -0,0 +1,22 @@
+const user = "felipsbreno"
+
+const links = [
+  {
+    label: "Github",
+    url: `https://github.com/${user}`,
+  },
+  {
+    label: "Twitter",
+    url: `https://twitter.com/${user}`,
+  },
+  {
+    label: "Instagram",
+    url: `https://www.instagram.com/${user}/`,
+  },
+  {
+    label: "Linkedin",
+    url: `https://www.linkedin.com/in/${user}/`,
+  },
+]
+
+export default links
