ddba11bedb84b9d9a6983aa2920f666fb52fd119
breno-felipe <brendo.filipe2050@gmail.com>
2021-11-20 18:13:29 -0300

    feat(menulinks): add component menulinks

---
+++ src/components/MenuLinks/index.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+import links from "./content"
+import * as S from "./styled"
+
+const MenuLinks = () => (
+  <S.MenuLinksWrapper>
+    <S.MenuLinksList>
+      {links.map((link, i) => (
+        <S.MenuLinksItem key={i} id={i}>
+          <S.MenuLinksLink activeClassName="active" to={link.url}>
+            {link.label}
+          </S.MenuLinksLink>
+        </S.MenuLinksItem>
+      ))}
+    </S.MenuLinksList>
+  </S.MenuLinksWrapper>
+)
+
+export default MenuLinks
