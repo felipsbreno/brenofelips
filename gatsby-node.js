bd0869e30db420465d5ffae4e4eb58f66e8ba0ce
breno-felipe <brendo.filipe2050@gmail.com>
2021-11-20 22:40:03 -0300

    feat(gatsby-node): add method for create new pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,5 @@
 const { createFilePath } = require(`gatsby-source-filesystem`)
+const path = require("path")
 
 // To add the slug field to each post
 exports.onCreateNode = ({ node, getNode, actions }) => {
@@ -19,4 +20,32 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
       value: `/${sliceSlug}`,
     })
   }
+}
+
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
+
+  return graphql(`
+    {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `).then(result => {
+    result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+      createPage({
+        path: node.fields.slug,
+        component: path.resolve("./src/templates/blog-post.js"),
+        context: {
+          slug: node.fields.slug,
+        },
+      })
+    })
+  })
 }
