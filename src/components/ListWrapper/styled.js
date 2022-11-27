7c99109adc8285ada92021e22047b0514f786ea6
felipsbreno <brendo.filipe2050@gmail.com>
2022-11-26 22:33:55 -0300

    feat(listwrapper): add ListWrapper to change the mode visualization
    modes: grid and list

---
+++ src/components/ListWrapper/styled.js
@@ -0,0 +1,11 @@
+import styled from "styled-components"
+
+export const ListWrapper = styled.section`
+  body#grid & {
+    border-bottom: 1px solid var(--borders);
+    display: grid;
+    grid-area: posts;
+    grid-gap: 1px;
+    grid-template-columns: repeat(auto-fit, minmax(380px, 1fr));
+  }
+`
